8a4822a1eb86757f8d3a6c8b719c76e4ae7ea470
Amy Lopes <111661@student.sae.edu.au>
2016-03-18 10:59:18 +1100

    Initial Creation

---
+++ LOL/Assets/Scripts/MiniGameScripts/Dewlap.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Dewlap : MonoBehaviour
+{
+
+    public GameObject dewlap;
+    public Collider dewlapCollider;
+    float timeLeft = 15;
+
+    public GameObject gameOver;
+    bool stop;
+
+    private Text textRef2;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        Data.control.points = 0;
+        textRef2 = GameObject.Find("Timer").GetComponent<Text>();
+        stop = false;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!stop)
+        {
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+                GetPointsValue();
+                stop = true;
+                gameOver.SetActive(true);
+
+            }
+
+            Debug.Log(dewlap.transform.position.x);
+
+            if (dewlap.transform.position.x < 1.8)
+            {
+                dewlap.transform.Translate((float)-0.01, (float)0.01, 0);
+            }
+            else if(dewlap.transform.position.x > 1.8)
+            {
+                Debug.Log("Stop");
+            }
+
+            if (Input.GetMouseButtonDown(0) && dewlap.transform.position.x > 1.4)
+            {
+                Data.control.energy--;
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit))
+                    if (hit.collider == dewlapCollider)
+                        dewlap.transform.Translate((float)0.05, (float)-0.05, 0);
+            }
+            else if(dewlap.transform.position.x < 1.4)
+            {
+                Debug.Log("STOP");
+            }
+
+
+        }
+
+        textRef2.text = "Timer = " + (int)timeLeft;
+
+    }
+
+    void GetPointsValue()
+    {
+        if(dewlap.transform.position.x < 1.9 && dewlap.transform.position.x > 1.7)
+        {
+            Data.control.points = 5;
+        }
+        else if(dewlap.transform.position.x < 1.7 && dewlap.transform.position.x > 1.5)
+        {
+            Data.control.points = 10;
+        }
+        else if(dewlap.transform.position.x < 1.5 && dewlap.transform.position.x > 1.2)
+        {
+            Data.control.points = 20;
+        }
+    }
+
+
+}
+
---
+++ LOL/Assets/Scripts/MiniGameScripts/WhackALizardController.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class WhackALizardController : MonoBehaviour
+{
+    public static WhackALizardController instance;
+    [SerializeField]
+    List<GameObject> spawnPoints = new List<GameObject>();
+
+
+	public GameObject lizard;
+	public GameObject tail;
+
+    public float baseSpawnTime;
+    float spawnTime;
+
+    public GameObject gameOver;
+    bool stop;
+    float timeLeft = 20;
+    private Text textRef2;
+    private Text textRef3;
+
+
+    void Awake()
+    {
+        instance = this;
+        GetRandomSpawnTime();
+
+    }
+
+    void Start()
+    {
+        Data.control.points = 0;
+        stop = false;
+        textRef2 = GameObject.Find("Timer").GetComponent<Text>();
+        textRef3 = GameObject.Find("Points").GetComponent<Text>();
+
+    }
+
+    void Update()
+    {
+        if (!stop)
+        {
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+                stop = true;
+                gameOver.SetActive(true);
+
+
+            }
+            if (spawnTime <= 0)
+            {
+                Spawn();
+            }
+            else
+            {
+                spawnTime -= Time.deltaTime;
+            }
+        }
+        textRef2.text = "Timer = " + (int)timeLeft;
+        textRef3.text = "Points = " + Data.control.points;
+
+    }
+
+    void GetRandomSpawnTime()
+    {
+        spawnTime = Random.Range(baseSpawnTime * 0.8f, baseSpawnTime * 1.2f);
+    }
+
+    void Spawn()
+    {
+        GameObject temp = spawnPoints[Random.Range(0, spawnPoints.Count)];
+        Vector3 pos = temp.transform.position;
+        GameObject tempLiz;
+        if (temp.GetComponent<WhackHole>().isOccupied)
+        {
+            Spawn();
+        }
+        else
+        {
+            bool spawnTail = (Random.Range(0, 4) == 1) ? true : false;
+            if (spawnTail)
+            {
+                tempLiz = Instantiate(tail, pos, Quaternion.identity) as GameObject;
+            }
+            else
+            {
+                tempLiz = Instantiate(lizard, pos, Quaternion.identity) as GameObject;
+            }
+            tempLiz.GetComponent<WhackLizard>().Init(temp);
+            temp.GetComponent<WhackHole>().isOccupied = true;
+            GetRandomSpawnTime();
+        }
+    }
+}
